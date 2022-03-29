49bb47877c63c5d0a7bd1f6b7330d52be798279d
Jonny Hodgson <s223358@uos.ac.uk>
2022-03-29 17:07:58 +0100

    Update PrintTransactionsReport.java

--- PrintTransactionsReport.java
+++ PrintTransactionsReport.java
@@ -5,10 +5,12 @@ public class PrintTransactionsReport {
 public static void printReport(String[] args) throws FileNotFoundException {
 
 System.out.println("Here's Your Report: \n");
+//Reads transactions file 
 File myObj = new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt");
 Scanner myReader = new Scanner(myObj);
 while (myReader.hasNextLine()) {
 String data = myReader.nextLine();
+//Outputs data 
 System.out.println(data);
 	}
 
