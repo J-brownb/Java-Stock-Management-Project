d1534b2b8c741bccdafec99d0f36a0a19cd61eeb
Jonny Hodgson <s223358@uos.ac.uk>
2022-03-30 10:03:34 +0100

    transac

--- PrintTransactionsReport.java
+++ PrintTransactionsReport.java
@@ -1,19 +1,21 @@
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException; 
-public class PrintTransactionsReport {
-public static void printReport(String[] args) throws IOException {
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
+public class ReadTransactionsTxt {
+public static void main(String[] args) {
 
+	try { 
+File myObj = new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt");
+Scanner myReader = new Scanner(myObj);
+while (myReader.hasNextLine()) {
+String data = myReader.nextLine();
+System.out.println(data);
+	}
 
-		FileWriter myWriter = new FileWriter ("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt");
-		myWriter.write("id,description,qtySold,amount,stockRemaining,transactionType " + System.getProperty( "line.separator" ));
-		myWriter.close();
-		System.out.println("Success");
-			System.out.println("Here's Your Report: \n");
-
-		
-		}
-			}
-
-		
-	
+myReader.close();
+} catch (FileNotFoundException e) {
+	System.out.println("An Error Occured.");
+e.printStackTrace();
+}
+}
+}
