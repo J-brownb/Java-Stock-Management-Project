c2110722a2344920a9482a09433fbf7fe9fa6a79
Jonny Hodgson <94555970+J-brownb@users.noreply.github.com>
2022-03-30 21:15:38 +0100

    work

--- WriteItemsTxt.java
+++ WriteItemsTxt.java
@@ -1,4 +1,8 @@
+package uk.ac.uos.i2p.s223358;
+
+import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner; // program uses class Scanner
 import java.io.IOException;
 public class WriteItemsTxt {
@@ -11,7 +15,21 @@ FileWriter myWriter = new FileWriter ("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitH
 FileWriter myWriter2 = new FileWriter ("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt", true);
 
 
-//Taking the ID
+////Taking the ID
+////need to autogenerate it, 5 digit number, incramenting by 1 
+////scan the items.txt file 
+//	Scanner s = new Scanner(new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\items.txt"));
+////create an array list with each new line of the items.txt file 
+//	ArrayList<String> list = new ArrayList<String>();
+//	while (s.hasNextLine()){
+//	    list.add(s.nextLine());
+//	}
+//	s.close();
+//	
+//	
+//	String[] vals = list.split("\\s+");
+
+
 Scanner input = new Scanner(System.in);
 System.out.print("Enter Item ID (format 00000):");
 int ID = input.nextInt(); 
@@ -35,7 +53,8 @@ System.out.print("Total Price Is: " + totalPrice + "\n");
 
 //Writing to the items.txt file and the transactions.txt file
 myWriter.write(ID + "," + Description + "," +  unitPrice + "," +  qtyInStock + "," +  totalPrice + System.getProperty( "line.separator" ));
-myWriter2.write(ID + "," + Description + "," +  unitPrice + "," +  qtyInStock + "," +  totalPrice + System.getProperty( "line.separator" ));
+//qty sold and amount are 0 as the item has just been added so none sold yet
+myWriter2.write(ID + "," + Description + "," +  "0" + "," + "0" + qtyInStock + "," +  "Added" + System.getProperty( "line.separator" ));
 myWriter2.close();
 myWriter.close();
 	} catch (IOException e) {
