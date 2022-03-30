ed07eef50a202554c831336728d6aa5972805ea4
Jonny Hodgson <s223358@uos.ac.uk>
2022-03-30 10:00:14 +0100

    work

--- PrintTransactionsReport.java
+++ PrintTransactionsReport.java
@@ -1,18 +1,19 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner; 
+import java.io.FileWriter;
+import java.io.IOException; 
 public class PrintTransactionsReport {
-public static void printReport(String[] args) throws FileNotFoundException {
+public static void printReport(String[] args) throws IOException {
 
-System.out.println("Here's Your Report: \n");
-//Reads transactions file 
-File myObj = new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt");
-Scanner myReader = new Scanner(myObj);
-while (myReader.hasNextLine()) {
-String data = myReader.nextLine();
-//Outputs data 
-System.out.println(data);
-	}
 
-myReader.close();
-} }
+		FileWriter myWriter = new FileWriter ("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt");
+		myWriter.write("id,description,qtySold,amount,stockRemaining,transactionType " + System.getProperty( "line.separator" ));
+		myWriter.close();
+		System.out.println("Success");
+			System.out.println("Here's Your Report: \n");
+
+		
+		}
+			}
+
+		
+	
--- UpdateQty.java
+++ UpdateQty.java
@@ -47,6 +47,10 @@ public static void update(String[] args) throws IOException {
 //print item name (at pos 0), and current stock levels (at pos 3)
     System.out.println("Item name: " + productdetail.get(0));
     System.out.println("Amount currently in stock: " + productdetail.get(3));
+    //collect old qty for updating later on
+    String oldqtystring = productdetail.get(3);
+    int oldqty = Integer.parseInt(oldqtystring);
+    
 
 //ask for new stock qty, save new qty inputted by user as new variable (new qty) 
 	System.out.print("\n" + "Please Enter the New Qauntity for Item " + (Item) + ":"+ "\n");
@@ -54,9 +58,8 @@ public static void update(String[] args) throws IOException {
 //setting up new string called productstrng based on newqty variable, set qty in arraylist to match this
 	String productstrng = String.valueOf(newqty);
     productdetail.set(3, productstrng);
-	System.out.println("New item details: " + productdetail + "\n" + "Your change has been accepted and the records have been updated! \n");
+	System.out.println("New item details: " + productdetail + "\n" + "Your change has been accepted and your Items.txt file has been updated! \n");
 
-	
 	//overwrite current arraylist item with new details inputted by user, remove square brackets on either side
 	list.set(Item, "" + (productdetail.toString().substring(1, productdetail.toString().length() - 1)));
 		  
@@ -69,7 +72,49 @@ public static void update(String[] args) throws IOException {
 		}
 	myWriter.close();
 	
-}}
+    if (oldqty > newqty) {
+    	
+    	//working out amount sold by subtracting new qty from old qty
+    	int qtysold = oldqty - newqty;
+    	//working out stock remaining by using newqty value given by user
+    	int stockremaining = newqty;
+    	//taking ID from array list  	
+    	String id = productdetail.get(0);
+    	//taking desc from array list
+    	String desc = productdetail.get(1);
+    	//taking amount per unit from array list and converting to double
+    	String amountstrng = productdetail.get(2);
+    	double cost = Double.parseDouble(amountstrng); 
+    	//working out amount of revenue by multiplying cost per unit by qty sold 
+    	double amount = cost * qtysold;
+    	//writing all this as an entry to transactions.txt   	
+    	
+    	FileWriter myWriter2 = new FileWriter ("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt", true);
+    	myWriter2.write(id + "," + desc + "," + qtysold + "," + amount + "," + stockremaining + ",Updated" + System.getProperty( "line.separator" ));
+    	myWriter2.close();
+
+
+     } else {
+    	 
+    	//taking ID from array list  	
+     	String id = productdetail.get(0);
+     	//taking desc from array list
+     	String desc = productdetail.get(1);
+     	//qty sold will be 0 as new qty is larger than oldqty
+     	//amount will be 0 as no revenue is created 
+    	//stockremaining is simply the user inputted value 'newqty'
+    	//writing all this as an entry to transactions.txt   	
+    	FileWriter myWriter4 = new FileWriter ("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt", true);
+    	myWriter4.write(id + "," + desc + "," + "0" + "," + "0" + "," + newqty + ",Updated" + System.getProperty( "line.separator" ));
+    	myWriter4.close();
+     	
+	}
+  	
+    	}}
+    	
+	
+	
+
 
 	//WORKS BUT THERE ARE SPACES IN THE EDITED ARRAY LIST ITEM
 
