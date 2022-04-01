0a7f8898b75bf50cbbac46ada8bfea8f67f72ca4
Jonny Hodgson <94555970+J-brownb@users.noreply.github.com>
2022-04-01 13:41:57 +0100

    GGI

--- JavaGUIExamples/DialogExample.java
+++ JavaGUIExamples/DialogExample.java
@@ -7,7 +7,9 @@ public class DialogExample {
         JFrame f= new JFrame();  
         d = new JDialog(f , "Dialog Example", true);  
         d.setLayout( new FlowLayout() );  
-        JButton b = new JButton ("OK");  
+        JButton b = new JButton ("Enter");  
+        JButton b2 = new JButton ("Leave");  
+
         b.addActionListener ( new ActionListener()  
         {  
             public void actionPerformed( ActionEvent e )  
@@ -15,11 +17,25 @@ public class DialogExample {
                 DialogExample.d.setVisible(false);  
             }  
         });  
-        d.add( new JLabel ("Click button to continue."));  
+        d.add( new JLabel ("Welcome"));  
         d.add(b);   
         d.setSize(300,300);    
         d.setVisible(true);  
-    }  
+        
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Welcome to my program");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
     public static void main(String args[])  
     {  
         new DialogExample();  
