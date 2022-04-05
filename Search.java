ca33a036e9484eafa21dcaaeafe7c15ddcf64257
Jonny Hodgson <s223358@uos.ac.uk>
2022-04-05 10:42:22 +0100

    work

--- Search.java
+++ Search.java
@@ -1,74 +1,84 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner; 
 import java.io.IOException;
 public class Search {
 public static void searchItem(String[] args) throws IOException {
 	
-	
-//scan the items.txt file 
-	Scanner s = new Scanner(new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\items.txt"));
-//create an array list with each new line of the items.txt file 
-	ArrayList<String> list = new ArrayList<String>();
-	while (s.hasNextLine()){
-	    list.add(s.nextLine());
-	}
+	//scan the items.txt file 
+		Scanner s = new Scanner(new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\items.txt"));
+	//create an array list with each new line of the items.txt file 
+		ArrayList<String> list = new ArrayList<String>();
+		while (s.hasNextLine()){
+		    list.add(s.nextLine());
+		}
 		s.close();
 		
-		 
-		System.out.print("Enter Search Term");
-		Scanner in = new Scanner(System.in);
-		String searchterm = in.nextLine();
-	        
-	    
-        System.out.println(list);
 
-	    
-	    
-        System.out.println(list.contains(searchterm));
-        
-		System.out.println(list);
-		// find word in String
-		boolean val = list.contains(searchterm);
-		if(val)
-			System.out.println("String found: "+searchterm);
-		else 
-			System.out.println("string not found");
+
+		System.out.print(list + "\n\n");
+
+		
+		//Taking the Description 
+		System.out.print("Enter Product Description:");
+		Scanner in = new Scanner(System.in);
+		String var = in.nextLine();
+
+//using a for loop to iterate through 	
+	      for (String s1 : list){
+	          if (s1.contains(var)){
+	                System.out.println(s1);
+	          }}}}
+
+		
+		
+//		  boolean ans = list.contains(var);
+//		  
+//	        if (ans)
+//	            System.out.println("The list contains " + var);
+//	        else
+//	            System.out.println("The list does not contain " + var);
+//	  
+
+		
+		
+//		System.out.print("Enter Search Term:");
+//		Scanner in = new Scanner(System.in);
+//		String searchterm = in.nextLine();
+		
+//		   // prints true
+//        System.out.println(data.contains("item1"));
+// 
+//        // prints false
+//    
+//
+//		}
+//}		}}
+		
+
+//
+//        System.out.println(list);
+//
+//        System.out.println(searchterm);
+//        
+//        System.out.println(list.contains(searchterm));
+//
+//        boolean found = false;
+//
+//	    
+//        for(String x : list){
+//        	if(x == searchterm){
+//                found = true;
+//                break;
+//            }
+//        }
+//        
+//        System.out.println(found);
+
 
 
 //prompt new action from user 
-System.out.println("\nWhat Would You Like To Do Next? \n");
-store.main(args); 
-
-}}
-        
-            
-    
-//	System.out.print("Enter Product Description. Example Format 'item1':");
-//	Scanner in = new Scanner(System.in);
-//	String Desc = in.nextLine();
-//	
-//	if (Desc.length() < 4) {
-//        System.out.println("\n Sorry, your search term is too broad, use at least 4 characters of text");
-//    	System.out.print("Enter Product Description. Example Format 'item1':");
-//    	in = new Scanner(System.in);
-//    	Desc = in.nextLine();
-//	} else {
-//	
-//		if (list.contains(Desc)) {
-//	    	System.out.print("Found:");
-//			} else {
-//		    	System.out.print("Not Found:");
-//			}
-//	
-//	
-//	}}}
-//		
-
-//     }
-//  }
-//}
-//}
+//System.out.println("\nWhat Would You Like To Do Next? \n");
+//store.main(args); 
+//
+//}}
