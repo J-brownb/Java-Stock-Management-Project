7304c0c66046e8e4e9f9105d8d5c2aadd3e975e4
Jonny Hodgson <s223358@uos.ac.uk>
2022-04-18 10:13:23 +0100

    Update PrintTransactionsReport.java

--- PrintTransactionsReport.java
+++ PrintTransactionsReport.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class PrintTransactionsReport {
 public static void printReport(String[] args) throws IOException {
 
+	//reading contents of transactions.txt file into data variable 
 	File myObj = new File("C:\\Users\\Jonny\\OneDrive\\Desktop\\GitHub\\I2P\\Assignment\\assignmentfori2p\\transactions.txt");
 	Scanner myReader = new Scanner(myObj);
 	while (myReader.hasNextLine()) {
 	String data = myReader.nextLine();
+	//printing data variable 
 	System.out.println(data);
 		}
 
+	//closing reader
 	myReader.close();
 	
 	System.out.print("\n Report Printed");	
